2b264391817755fd8c028a40b5861b162a8bcd0d
Cam-4587 Conroy <cameronconroy@live.com>
2025-04-02 15:23:14 +0000

    func: Add First time buyer consideration for calculating stamp duty

--- assets/script.js
+++ assets/script.js
@@ -2,32 +2,56 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const stampDutyForm = document.getElementById("stampdutyform");
     const propertyPrice = document.getElementById("inlineFormInputGroup");
     const propertyLocation = document.getElementById("propertyLocation");
-    const engOrNI= document.getElementById("inlineRadio1");
+    const engOrNI = document.getElementById("inlineRadio1");
     const scotland = document.getElementById("inlineRadio2");
     const wales = document.getElementById("inlineRadio3");
     const firstTimeBuyer = document.getElementById("inlineCheckbox1");
+    const stampDuty= document.getElementById("stampduty")
 
     stampDutyForm.addEventListener("submit", function (e) {
         e.preventDefault();
         let propertyPriceValue = propertyPrice.value;
-        let sdlt;
-        if (propertyPriceValue < 125000) {
-            sdlt = 0;
+        if (engOrNI.checked) {
+            englandOrNorthernIreland(propertyPriceValue, firstTimeBuyer);
+        } else {
+            console.log("Hello World");
         }
-        else if (propertyPriceValue >= 125001 && propertyPriceValue <= 250000) {
-            sdlt = (propertyPriceValue - 125000) * 0.02;
-        }
-        else if (propertyPriceValue >= 250001 && propertyPriceValue <= 925000) {
-            sdlt = (250000 - 125000) * 0 + (propertyPriceValue - 250000) * 0.05;
-        }
-        else if (propertyPriceValue >= 925001 && propertyPriceValue <= 1500000) {
-            sdlt = (250000 - 125000) * 0.02 + (925000 - 250000) * 0.075 (propertyPriceValue - 925000) * 0.1;
-        }
-        else if (propertyPriceValue > 1500000) {
-            sdlt = (250000 - 125000) * 0.02 + + (925000 - 250000) * 0.075 + (1500000 - 925000) * 0.075 (propertyPriceValue - 1500000) * 1.2;
-        }
-        console.log(sdlt)
-    },
-    );
-});
 
+    });
+
+    function englandOrNorthernIreland(propertyPriceValue, firstTimeBuyer) {
+        let sdlt;
+        if (firstTimeBuyer.checked) {
+            if (propertyPriceValue < 300000) {
+                sdlt = 0;
+            } else if (propertyPriceValue >= 300001 && propertyPriceValue < 500000) {
+                sdlt = (propertyPriceValue - 300000) * 0.05;
+            } else {
+                if (propertyPriceValue < 125000) {
+                    sdlt = 0;
+                } else if (propertyPriceValue >= 125001 && propertyPriceValue <= 250000) {
+                    sdlt = (propertyPriceValue - 125000) * 0.02;
+                } else if (propertyPriceValue >= 250001 && propertyPriceValue <= 925000) {
+                    sdlt = (250000 - 125000) * 0.02 + (propertyPriceValue - 250000) * 0.05;
+                } else if (propertyPriceValue >= 925001 && propertyPriceValue <= 1500000) {
+                    sdlt = (250000 - 125000) * 0.02 + (925000 - 250000) * 0.075 + (propertyPriceValue - 925000) * 0.1;
+                } else if (propertyPriceValue > 1500000) {
+                    sdlt = (250000 - 125000) * 0.02 + (925000 - 250000) * 0.075 + (1500000 - 925000) * 0.075 + (propertyPriceValue - 1500000) * 0.12;
+                }
+            }
+        } else {
+            if (propertyPriceValue < 125000) {
+                sdlt = 0;
+            } else if (propertyPriceValue >= 125001 && propertyPriceValue <= 250000) {
+                sdlt = (propertyPriceValue - 125000) * 0.02;
+            } else if (propertyPriceValue >= 250001 && propertyPriceValue <= 925000) {
+                sdlt = (250000 - 125000) * 0.02 + (propertyPriceValue - 250000) * 0.05;
+            } else if (propertyPriceValue >= 925001 && propertyPriceValue <= 1500000) {
+                sdlt = (250000 - 125000) * 0.02 + (925000 - 250000) * 0.075 + (propertyPriceValue - 925000) * 0.1;
+            } else if (propertyPriceValue > 1500000) {
+                sdlt = (250000 - 125000) * 0.02 + (925000 - 250000) * 0.075 + (1500000 - 925000) * 0.075 + (propertyPriceValue - 1500000) * 0.12;
+            }
+        }
+        stampDuty.textContent = `£ ${sdlt}`;
+    }
+});
